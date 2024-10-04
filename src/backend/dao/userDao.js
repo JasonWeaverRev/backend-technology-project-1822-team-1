b8d7742ddf5591bd9cb8bec4d6ff40c842234ecf
Aaron Cordeau <akcordeau@gmail.com>
2024-10-04 00:52:34 -0400

    Add userDao/encounterDao

--- src/backend/dao/userDao.js
+++ src/backend/dao/userDao.js
@@ -11,6 +11,8 @@ const {
   DeleteCommand,
 } = require("@aws-sdk/lib-dynamodb");
 
+const encounterDao = require("../dao/encounterDao");
+
 const client = new DynamoDBClient({ region: "us-east-1" });
 
 const documentClient = DynamoDBDocumentClient.from(client);
@@ -18,7 +20,6 @@ const documentClient = DynamoDBDocumentClient.from(client);
 const TableName = "Dungeon_Delver_Users";
 
 const getUserByEmail = async (email) => {
-  console.log("inside getUserByEmail");
   try {
     const command = new GetCommand({
       TableName,
@@ -26,8 +27,19 @@ const getUserByEmail = async (email) => {
     });
 
     const data = await documentClient.send(command);
-    return data.Item || null;
+
+    const userData = data.Item;
+
+    if (userData && userData.encounters && userData.encounters.length > 0) {
+      const encounterData = await encounterDao.getBatchEncountersbyId(
+        userData.encounters
+      );
+      userData.encounters = encounterData;
+    }
+
+    return userData || null;
   } catch (err) {
+    console.error(err);
     throw { status: 500, message: "Error retrieving  user by email" };
   }
 };
@@ -48,6 +60,15 @@ const getUserByUsername = async (username) => {
 
     const data = await documentClient.send(command);
 
+    const encounterIds = [];
+    data.Items[0].encounters.L.forEach((idx) => {
+      encounterIds.push(idx.S);
+    });
+
+    const encounterData = await encounterDao.getBatchEncountersbyId(
+      encounterIds
+    );
+
     const userData = (data.Items || []).map((item) => ({
       password: item.password.S,
       about_me: item.about_me.S,
@@ -57,7 +78,7 @@ const getUserByUsername = async (username) => {
       email: item.email.S,
       profile_pic: item.profile_pic.S,
       encounter_campaigns: item.encounter_campaigns.L,
-      encounters: item.encounters.L,
+      encounters: encounterData,
       interacted_posts: item.interacted_posts.L,
       forum_posts: item.forum_posts.L,
     }));
