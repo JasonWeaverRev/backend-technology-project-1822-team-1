2edd3b84108cf7d7d5681f387c7a3dedf9f24b76
Aaron Cordeau <akcordeau@gmail.com>
2024-10-04 11:45:56 -0400

    Extract processing from dao into service

--- src/backend/dao/userDao.js
+++ src/backend/dao/userDao.js
@@ -28,16 +28,7 @@ const getUserByEmail = async (email) => {
 
     const data = await documentClient.send(command);
 
-    const userData = data.Item;
-
-    if (userData && userData.encounters && userData.encounters.length > 0) {
-      const encounterData = await encounterDao.getBatchEncountersbyId(
-        userData.encounters
-      );
-      userData.encounters = encounterData;
-    }
-
-    return userData || null;
+    return data.Item || null;
   } catch (err) {
     console.error(err);
     throw { status: 500, message: "Error retrieving  user by email" };
@@ -60,30 +51,45 @@ const getUserByUsername = async (username) => {
 
     const data = await documentClient.send(command);
 
-    const encounterIds = [];
-    data.Items[0].encounters.L.forEach((idx) => {
-      encounterIds.push(idx.S);
-    });
+    // const encounterIds = [];
+    // data.Items[0].encounters.L.forEach((idx) => {
+    //   encounterIds.push(idx.S);
+    // });
 
-    const encounterData = await encounterDao.getBatchEncountersbyId(
-      encounterIds
-    );
+    // const encounterData = await encounterDao.getBatchEncountersbyId(
+    //   encounterIds
+    // );
 
-    const userData = (data.Items || []).map((item) => ({
-      password: item.password.S,
-      about_me: item.about_me.S,
-      role: item.role.S,
-      creation_time: item.creation_time.S,
-      username: item.username.S,
-      email: item.email.S,
-      profile_pic: item.profile_pic.S,
-      encounter_campaigns: item.encounter_campaigns.L,
-      encounters: encounterData,
-      interacted_posts: item.interacted_posts.L,
-      forum_posts: item.forum_posts.L,
-    }));
+    // const encounterCampaigns = [];
+    // data.Items[0].encounter_campaigns.L.forEach((idx) => {
+    //   encounterCampaigns.push(idx.S);
+    // });
 
-    return userData[0] || null;
+    // const interactedPosts = [];
+    // data.Items[0].interacted_posts.L.forEach((idx) => {
+    //   interactedPosts.push(idx.S);
+    // });
+
+    // const forumPosts = [];
+    // data.Items[0].forum_posts.L.forEach((idx) => {
+    //   forumPosts.push(idx.S);
+    // });
+
+    // const userData = (data.Items || []).map((item) => ({
+    //   password: item.password.S,
+    //   about_me: item.about_me.S,
+    //   role: item.role.S,
+    //   creation_time: item.creation_time.S,
+    //   username: item.username.S,
+    //   email: item.email.S,
+    //   profile_pic: item.profile_pic.S,
+    //   encounter_campaigns: encounterCampaigns,
+    //   encounters: encounterData,
+    //   interacted_posts: interactedPosts,
+    //   forum_posts: forumPosts,
+    // }));
+
+    return data.Items[0] || null;
   } catch (err) {
     throw { status: 500, message: "Error retrieving user by username" };
   }
--- src/backend/service/userService.js
+++ src/backend/service/userService.js
@@ -1,4 +1,5 @@
 const userDao = require("../dao/userDao");
+const encounterDao = require("../dao/encounterDao");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { logger } = require("../utils/logger");
@@ -23,34 +24,86 @@ const loginUser = async (identifier, password) => {
       throw { status: 401, message: "Invalid username/email or password" };
     }
 
-    // console.log(user.encounters[0]);
-
-    // const userEncounterData = await encounterDao.getEncounterById(
-    //   user.encounters[0]
-    // );
-    // console.log(userEncounterData);
+    const processedUser = identifier.includes("@")
+      ? await processByEmail(user)
+      : await processByUsername(user);
 
     const token = jwt.sign(
       {
-        username: user.username,
-        email: user.email,
-        role: user.role,
-        about_me: user.about_me,
-        encounter_campaigns: user.encounter_campaigns,
-        encouters: user.encounters,
-        forum_posts: user.forum_posts,
-        interacted_posts: user.interacted_posts,
-        profile_pic: user.profile_pic,
+        username: processedUser.username,
+        email: processedUser.email,
+        role: processedUser.role,
+        about_me: processedUser.about_me,
+        encounter_campaigns: processedUser.encounter_campaigns,
+        encouters: processedUser.encounters,
+        forum_posts: processedUser.forum_posts,
+        interacted_posts: processedUser.interacted_posts,
+        profile_pic: processedUser.profile_pic,
       },
       secret,
       { expiresIn: "1h" }
     );
 
-    logger.info(`Successful login by ${user.username}`);
+    logger.info(`Successful login by ${processedUser.username}`);
     return token;
   } catch (err) {
+    console.error(err);
     throw err.status ? err : { status: 500, message: "Internal server error" };
   }
+};
+
+const processByEmail = async (user) => {
+  const encounterData = [];
+  if (user && user.encounters && user.encounters.length > 0) {
+    encounterData = await encounterDao.getBatchEncountersbyId(user.encounters);
+    user.encounters = encounterData;
+  }
+
+  return user;
+};
+
+const processByUsername = async (user) => {
+  const encounterIds = [];
+  user.encounters.L.forEach((idx) => {
+    encounterIds.push(idx.S);
+  });
+
+  const encounterData = [];
+
+  if (user && user.encounters && user.encounters.length > 0) {
+    encounterData = await encounterDao.getBatchEncountersbyId(encounterIds);
+  }
+
+  const encounterCampaigns = [];
+  user.encounter_campaigns.L.forEach((idx) => {
+    encounterCampaigns.push(idx.S);
+  });
+
+  const interactedPosts = [];
+  user.interacted_posts.L.forEach((idx) => {
+    interactedPosts.push(idx.S);
+  });
+
+  const forumPosts = [];
+  user.forum_posts.L.forEach((idx) => {
+    forumPosts.push(idx.S);
+  });
+
+  const processedUser = {
+    password: user.password.S,
+    about_me: user.about_me.S,
+    role: user.role.S,
+    creation_time: user.creation_time.S,
+    username: user.username.S,
+    email: user.email.S,
+    profile_pic: user.profile_pic.S,
+    encounter_campaigns: encounterCampaigns,
+    encounters: encounterData,
+    interacted_posts: interactedPosts,
+    forum_posts: forumPosts,
+  };
+
+  return processedUser;
 };
 
 module.exports = {
