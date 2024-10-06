b5f1c947d0c6e2a2a274f2ad5845adbb87d7a186
Aaron Cordeau <akcordeau@gmail.com>
2024-10-05 20:24:37 -0400

    Add encounterController

---
+++ src/backend/controller/encounterController.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const encounterService = require("../service/encounterService");
+
+router.get("/monsters", async (req, res) => {
+  const challengeRating = req.query.challenge;
+  try {
+    const monsters = await encounterService.getMonstersByChallengeRating(
+      challengeRating
+    );
+    res.status(200).json({ monsters });
+  } catch (err) {
+    res.status(err.status || 400).json({ message: err.message });
+  }
+});
--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const { logger } = require("./backend/utils/logger");
+const encounterController = require("./backend/controller/encounterController");
 
 app.use(express.json());
+
+app.use("/api/encounters", encounterController);
 
 const PORT = process.env.PORT || 3000;
 
