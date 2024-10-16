a2b2d2f60397afa7e99f0739b347c6089e0e70d4
rickyly1 <lyr5@uci.edu>
2024-10-15 21:53:48 -0700

    wrote function to get user account info from auth token

--- src/backend/controller/accountController.js
+++ src/backend/controller/accountController.js
@@ -45,28 +45,29 @@ router.get("/email", async (req, res) => {
 });
 */
 
-/* GET user by username
-router.get("/username", async (req, res) => {
-    const usernameQuery = req.query.username;
+// get user data based on their Auth token
+router.get("/profile", AuthMiddleware.verifyToken, async (req, res) => {
+    const username = req.user.username;
+    console.log("router layer ", username);
 
     try {
-        if (usernameQuery) {
-            const user = await AccountService.getUserByUsername(usernameQuery);
+        const user = await accountService.getUserByUsername(username);
+        const userProfile = {
+          email: user.email.S,
+          username: user.username.S,
+          about_me: user.about_me.S,
+          role: user.role.S,
+          creation_time: user.creation_time.S
+      };
 
-            if (user) {
-                return res.status(200).json({ message: "User found", user });
-            } else {
-                return res.status(404).json({ message: "User not found" });
-            }
-        } else {
-            return res.status(400).json({ message: "Username not provided" });
-        }
+      return res.status(200).json({ userProfile });
+
     } catch (error) {
         console.error("Error fetching user by username:", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 });
-*/
+
 
 // POST user registration
 // user registration
