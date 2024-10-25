0a4a82b17ed49ae8ab37e7b9d67eed98ec5811a7
Aaron Cordeau <akcordeau@gmail.com>
2024-10-25 12:12:33 -0400

    remove critical test from server.js

--- src/server.js
+++ src/server.js
@@ -18,11 +18,6 @@ const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 app.use(express.json({ limit: "5mb" }));
 
-if (!process.env.JWT_SECRET || !process.env.DATABASE_URL) {
-  console.error("Critical environment variables are missing.");
-  process.exit(1); // Exit the application if critical variables are missing
-}
-
 function loggerMiddleware(req, res, next) {
   logger.info(`Incoming ${req.method} : ${req.url}`);
   next();
